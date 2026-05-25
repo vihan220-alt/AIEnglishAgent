5e245748db1a250682fd9c5db6650db4e142857f
vihan220-alt <vihan220@gmail.com>
2026-05-25 11:53:37 +0530

    Update style.py

--- style.py
+++ style.py
@@ -1,30 +1,8 @@
-import streamlit as st
-
-def apply_custom_theme():
-    st.markdown("""
-        <style>
-        .stApp {
+.stApp {
             background-color: #000000 !important;
-            /* Using a high-quality robot-themed background */
-            background-image: url("https://img.freepik.com/free-vector/digital-technology-background-with-abstract-robot-head_23-2148425143.jpg");
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
+            /* Replace the link below with one of the URLs above */
+            background-image: url("https://cdn-icons-png.flaticon.com/512/1000/1000966.png");
+            background-size: 80px;
+            background-repeat: repeat;
+            opacity: 0.9;
         }
-        
-        /* Darken the overlay so white text is always readable */
-        .stApp::before {
-            content: "";
-            position: absolute;
-            top: 0; left: 0; width: 100%; height: 100%;
-            background: rgba(0, 0, 0, 0.7);
-            z-index: -1;
-        }
-
-        h1, h2, p, div, span, label { color: #ffffff !important; }
-        
-        [data-testid="stSidebar"] {
-            background-color: #1a1a1a !important;
-        }
-        </style>
-        """, unsafe_allow_html=True)
