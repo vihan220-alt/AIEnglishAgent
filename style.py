1279108ed19c8ae2dd75774c8e4c3562cd933686
vihan220-alt <vihan220@gmail.com>
2026-05-25 10:33:16 +0530

    Update style.py

--- style.py
+++ style.py
@@ -3,15 +3,13 @@ import streamlit as st
 def apply_custom_theme():
     st.markdown("""
         <style>
-        .stApp {
-            background-color: #000000;
-            background-image: url("https://cdn-icons-png.flaticon.com/512/2040/2040901.png");
-            background-size: 80px;
-        }
-        /* Make all text white and input fields black */
+        .stApp { background-color: #000000; }
         h1, h2, p, div, span, label { color: white !important; }
-        .stTextInput > div > div > input { background-color: black !important; color: white !important; }
-        .stChatInput textarea { background-color: black !important; color: white !important; }
-        .stButton button { background-color: #222 !important; color: white !important; }
+        .stTextInput > div > div > input, .stChatInput textarea { 
+            background-color: #222 !important; color: white !important; 
+        }
+        .stButton button { background-color: #333 !important; color: white !important; border: 1px solid #555; }
+        /* Robot background */
+        .stApp { background-image: url("https://cdn-icons-png.flaticon.com/512/2040/2040901.png"); background-size: 80px; }
         </style>
         """, unsafe_allow_html=True)
