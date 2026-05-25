16c342034beb79e37c4b0e5e73c92f599b5628d4
vihan220-alt <vihan220@gmail.com>
2026-05-25 10:28:02 +0530

    Update style.py

--- style.py
+++ style.py
@@ -4,11 +4,14 @@ def apply_custom_theme():
     st.markdown("""
         <style>
         .stApp {
-            background-color: #0e1117;
+            background-color: #000000;
             background-image: url("https://cdn-icons-png.flaticon.com/512/2040/2040901.png");
             background-size: 80px;
         }
-        h1, h2, p, div, span { color: white !important; }
-        .stButton button { color: white !important; }
+        /* Make all text white and input fields black */
+        h1, h2, p, div, span, label { color: white !important; }
+        .stTextInput > div > div > input { background-color: black !important; color: white !important; }
+        .stChatInput textarea { background-color: black !important; color: white !important; }
+        .stButton button { background-color: #222 !important; color: white !important; }
         </style>
         """, unsafe_allow_html=True)
