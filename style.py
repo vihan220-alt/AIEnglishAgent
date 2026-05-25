2f336cd3dadf7debb11c93808074fe1ef15dcb2c
vihan220-alt <vihan220@gmail.com>
2026-05-25 11:49:23 +0530

    Update style.py

--- style.py
+++ style.py
@@ -5,8 +5,13 @@ def apply_custom_theme():
         <style>
         .stApp {
             background-color: #000000 !important;
-            color: #ffffff !important;
+            background-image: url("https://cdn-icons-png.flaticon.com/512/2040/2040901.png");
+            background-size: 80px;
+            background-repeat: repeat;
         }
-        h1, h2, p, div, span { color: #ffffff !important; }
+        h1, h2, p, div, span, label { color: white !important; }
+        .stTextInput > div > div > input, .stChatInput textarea { 
+            background-color: #222 !important; color: white !important; 
+        }
         </style>
         """, unsafe_allow_html=True)
