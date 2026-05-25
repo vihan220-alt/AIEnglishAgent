1e3cb5154a19ee93515b9dad38ca33fc8d77338b
vihan220-alt <vihan220@gmail.com>
2026-05-25 09:32:11 +0530

    Update message.py

--- message.py
+++ message.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+def display_chat():
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    for msg in st.session_state.messages:
+        with st.chat_message("user"):
+            st.markdown(msg)
+
+    if prompt := st.chat_input("Type your message here..."):
+        st.session_state.messages.append(prompt)
+        st.rerun()
