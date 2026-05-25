17b80c958b0e87d944f1489fee4c6c247db705c4
vihan220-alt <vihan220@gmail.com>
2026-05-25 11:51:14 +0530

    Update style.py

--- style.py
+++ style.py
@@ -3,15 +3,20 @@ import streamlit as st
 def apply_custom_theme():
     st.markdown("""
         <style>
+        /* Force dark background and repeating robot icon */
         .stApp {
             background-color: #000000 !important;
             background-image: url("https://cdn-icons-png.flaticon.com/512/2040/2040901.png");
-            background-size: 80px;
+            background-size: 100px;
             background-repeat: repeat;
         }
+        
+        /* Make text white and sidebar distinct */
         h1, h2, p, div, span, label { color: white !important; }
-        .stTextInput > div > div > input, .stChatInput textarea { 
-            background-color: #222 !important; color: white !important; 
+        
+        [data-testid="stSidebar"] {
+            background-color: #111111 !important;
+            border-right: 1px solid #333;
         }
         </style>
         """, unsafe_allow_html=True)
