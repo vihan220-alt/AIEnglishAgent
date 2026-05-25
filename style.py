383dc82a0473b7c43a58c29eaeb5ce3de02181b9
vihan220-alt <vihan220@gmail.com>
2026-05-25 10:21:46 +0530

    Update style.py

--- style.py
+++ style.py
@@ -5,12 +5,10 @@ def apply_custom_theme():
         <style>
         .stApp {
             background-color: #0e1117;
-            /* New robot face below */
             background-image: url("https://cdn-icons-png.flaticon.com/512/2040/2040901.png");
             background-size: 80px;
-            background-repeat: repeat;
-            opacity: 0.8;
         }
-        h1, h2, p { color: white !important; }
+        h1, h2, p, div, span { color: white !important; }
+        .stButton button { color: white !important; }
         </style>
         """, unsafe_allow_html=True)
